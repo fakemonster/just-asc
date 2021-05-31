527ce5eb0a7e3901aa87c8e222c863ce7caae64a
Joe Thel <jgraythel@gmail.com>
2021-05-31 15:39:46 -0700

    ✅ add a bunch of lines to sample

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,79 @@
 mod asc;
 
+fn borders(grid: &mut asc::Grid) {
+    grid.line(0., 0., 100., 0.);
+    grid.line(100., 0., 100., 100.);
+    grid.line(100., 100., 0., 100.);
+    grid.line(0., 0., 0., 100.);
+}
+
+fn offset_x_line(grid: &mut asc::Grid, offset: f64, x1: f64, y1: f64, x2: f64, y2: f64) {
+    grid.line(x1 + offset, y1, x2 + offset, y2);
+}
+
+fn sliding_angles(grid: &mut asc::Grid, frame: usize) {
+    let angle = (2. * 3.14159 / 50.) * frame as f64;
+    let offset = angle.cos() * 4.;
+
+    offset_x_line(grid, offset, 40., 3., 60., 3.);
+
+    offset_x_line(grid, offset, 15., 5., 1.5, 8.);
+    offset_x_line(grid, offset, 20., 5., 8., 9.);
+    offset_x_line(grid, offset, 25., 5., 12.5, 10.);
+    offset_x_line(grid, offset, 30., 5., 18., 11.);
+    offset_x_line(grid, offset, 35., 5., 24.5, 12.);
+    offset_x_line(grid, offset, 40., 5., 32., 13.);
+    offset_x_line(grid, offset, 45., 5., 41., 14.);
+    offset_x_line(grid, offset, 50., 5., 50., 15.);
+    offset_x_line(grid, offset, 55., 5., 59., 14.);
+    offset_x_line(grid, offset, 60., 5., 68., 13.);
+    offset_x_line(grid, offset, 65., 5., 75.5, 12.);
+    offset_x_line(grid, offset, 70., 5., 82., 11.);
+    offset_x_line(grid, offset, 75., 5., 87.5, 10.);
+    offset_x_line(grid, offset, 80., 5., 92., 9.);
+    offset_x_line(grid, offset, 85., 5., 98.5, 8.);
+}
+
+fn spinning_lines(grid: &mut asc::Grid, frame: usize) {
+    let angle = (2. * 3.14159 / 60.) * frame as f64;
+    let x = angle.cos();
+    let y = angle.sin();
+
+    grid.line(
+        50. - (x * 5.),
+        50. - (y * 5.),
+        50. + (x * 30.),
+        50. + (y * 30.),
+    );
+    grid.line(
+        50. - (y * 3.),
+        50. - (x * 3.),
+        50. + (y * 10.),
+        50. + (x * 10.),
+    );
+    grid.line(
+        70. - (y * 5.),
+        30. - (x * 5.),
+        20. + (y * 10.),
+        30. + (x * 10.),
+    );
+}
+
+fn circle_stuff(grid: &mut asc::Grid, frame: usize) {
+    let slow = (2. * 3.14159 / 120.) * frame as f64;
+    let x = slow.cos();
+    let y = slow.sin();
+
+    grid.circle(50. + (x * 10.), 50. + (y * 10.), 10.);
+    grid.circle(50. + (x * 5.), 85., 10. + (x * 10.));
+    grid.circle(50. + (x * 5.), 85., 10. + (y * 10.));
+}
+
 fn main() {
-    asc::draw(|grid, frame| {
-        let angle = (2. * 3.14159 / 60.) * frame as f64;
-        let angle_x = angle.cos();
-        let angle_y = angle.sin();
-        let slow = (2. * 3.14159 / 120.) * frame as f64;
-        let slow_x = slow.cos();
-        let slow_y = slow.sin();
-
-        grid.line(0., 0., 100., 0.);
-        grid.line(100., 0., 100., 100.);
-        grid.line(100., 100., 0., 100.);
-        grid.line(0., 0., 0., 100.);
-
-        grid.line(
-            50. - (angle_x * 5.),
-            50. - (angle_y * 5.),
-            50. + (angle_x * 30.),
-            50. + (angle_y * 30.),
-        );
-        grid.line(
-            50. - (angle_y * 3.),
-            50. - (angle_x * 3.),
-            50. + (angle_y * 10.),
-            50. + (angle_x * 10.),
-        );
-        grid.line(
-            70. - (angle_y * 5.),
-            30. - (angle_x * 5.),
-            20. + (angle_y * 10.),
-            30. + (angle_x * 10.),
-        );
-        grid.circle(50. + (slow_x * 10.), 50. + (slow_y * 10.), 10.);
-        grid.circle(50. + (slow_x * 5.), 85., 10. + (slow_x * 10.));
+    asc::draw(|grid: &mut asc::Grid, frame: usize| {
+        borders(grid);
+        sliding_angles(grid, frame);
+        spinning_lines(grid, frame);
+        circle_stuff(grid, frame);
     });
 }
